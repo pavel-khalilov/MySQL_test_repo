775180671fd22625014dfaa82d5e2755d9dde874
pavel-khalilov <pavel.khalilov.qa@gmail.com>
2025-06-30 16:16:36 -0400

    fix testEnvSecrets()

--- src/test/java/EnvSecretsTest.java
+++ src/test/java/EnvSecretsTest.java
@@ -5,7 +5,6 @@ import org.testng.annotations.Test;
 public class EnvSecretsTest {
     @Test
     public void testEnvSecrets() {
-        System.out.println("User: " + ProjectUtils.getUser());
         Assert.assertEquals(ProjectUtils.getUser(), "root");
     }
 }
